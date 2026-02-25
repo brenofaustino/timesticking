8c68e45df1f0b6efbe8be4f75d104085f830ce77
Breno Faustino <faustinobreno90@gmail.com>
2026-02-25 14:28:11 -0300

    first component and adjust component patterns

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
+import { Container } from './components/Container';
+import { Heading } from './components/Heading';
+import { Logo } from './components/Logo';
+
 import './styles/theme.css';
 import './styles/global.css';
-
-import {Container} from './components/Container';
-import {Heading} from './components/Heading';
 
 export function App() {
 
     return (
         <>
             <Container>
-                <Heading>LOGO</Heading>
+                <Logo></Logo>
             </Container>
             <Container>
                 <Heading>MENU</Heading>
