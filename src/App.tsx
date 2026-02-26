f268c399d277fda091f60baecced45617cb15237
Breno Faustino <faustinobreno90@gmail.com>
2026-02-26 13:18:46 -0300

    task input and timer component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Container } from './components/Container';
-import { Heading } from './components/Heading';
+// import { Heading } from './components/Heading';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
 
 export function App() {
 
@@ -17,10 +19,24 @@ export function App() {
                 <Menu/>
             </Container>
             <Container>
-                <Heading>FORM</Heading>
+                <CountDown/>
             </Container>
             <Container>
-                <Heading>FOOTER</Heading>
+                <form className='form' action=''>
+                    <div className='form-row'>
+                        <DefaultInput labelText='task' id='meu-input' type='text' placeholder='digite algo'/>
+                    </div>
+                    <div className='form-row'>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                    <div className='form-row'>
+                        <p>ciclos</p>
+                        <p>0 0 0 0 0</p>
+                    </div>
+                    <div className='form-row'>
+                        <button>Enviar</button>
+                    </div>
+                </form>
             </Container>
         </>
     )
